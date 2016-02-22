f9183f4011dcede16b36eb3762b68294dc79ca9b
Kerry Liu <kerryliu123@gmail.com>
2016-02-21 21:40:41 -0500

    whoops

--- src/threads/synch.c
+++ src/threads/synch.c
@@ -109,11 +109,9 @@ sema_try_down (struct semaphore *sema)
 void
 sema_up (struct semaphore *sema)
 {
-  enum intr_level old_level = intr_disable();
-
   ASSERT (sema != NULL);
 
-  old_level = intr_disable ();
+  enum intr_level old_level = intr_disable();
   if (!list_empty (&sema->waiters)) {
     list_sort(&sema->waiters, &thread_compare_priority, NULL);
     thread_unblock (list_entry (list_pop_front (&sema->waiters), struct thread, elem));
